2dc19c8eb09ac10a3cd2afd839e7cc0cde3ea9dd
matmorel <pub.imagiin@gmail.com>
2018-02-23 12:43:04 +0100

    create some DAO

--- default/src/dao/DaoOption.php
+++ default/src/dao/DaoOption.php
@@ -5,4 +5,25 @@ use simplon\entities\Option;
 use simplon\dao\Connect;
 
 class DaoOption {
+  public function vote(int $optionId){
+    try {
+      $query = Connect::getInstance()->prepare(
+        "UPDATE options SET count = count + 1 WHERE id = :id"
+      );
+      $query->bindValue(":id", $optionId);
+      $query->execute();
+
+      $query = Connect::getInstance()->prepare(
+        "SELECT * FROM options WHERE id = :id"
+      );
+      $query->bindValue(":id", $optionId);
+      $query->execute();
+
+      $option = $query->fetch();
+      return new Option($option["id"], $option["text"], $option["count"]);
+      
+    } catch (Exception $e) {
+      echo $e;
+    }
+  }
 }
--- default/src/dao/DaoPoll.php
+++ default/src/dao/DaoPoll.php
@@ -7,4 +7,48 @@ use simplon\dao\Connect;
 use simplon\dao\DaoOption;
 
 class DaoPoll {
+  public function addWithOptions(Poll $poll){
+    try {
+      $query = Connect::getInstance()->prepare(
+        "INSERT INTO polls(title) VALUES (:title)"
+      );
+      $query->bindValue(":title", $poll->get()["title"]);
+      $query->execute();
+
+      $pollId = Connect::getInstance()->lastInsertId();
+
+      for ($i=0; $i < count($poll->get()["options"]); $i++) {
+        $text = $poll->get()["options"][$i]["text"];
+
+        $query = Connect::getInstance()->prepare(
+          "INSERT INTO options(text, count, poll_id) VALUES (:text, 0, :poll_id)"
+        );
+        $query->bindValue(":text", $text);
+        $query->bindValue(":poll_id", $pollId);
+        $query->execute();
+      }
+
+    } catch (Exception $e) {
+      echo $e;
+    }
+  }
+  //   try {
+  //   $query = Connect::getInstance()->prepare(
+  //     "UPDATE options SET count = count + 1 WHERE id = :id"
+  //   );
+  //   $query->bindValue(":id", $optionId);
+  //   $query->execute();
+
+  //   $query = Connect::getInstance()->prepare(
+  //     "SELECT * FROM options WHERE id = :id"
+  //   );
+  //   $query->bindValue(":id", $optionId);
+  //   $query->execute();
+
+  //   $option = $query->fetch();
+  //   return new Option($option["id"], $option["text"], $option["count"]);
+    
+  // } catch (Exception $e) {
+  //   echo $e;
+  // }
 }
