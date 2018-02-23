0d59f204628c76a4e35c2c915eb926929170693e
matmorel <pub.imagiin@gmail.com>
2018-02-23 11:43:53 +0100

    create entities

--- default/src/entities/Option.php
+++ default/src/entities/Option.php
@@ -3,4 +3,21 @@
 namespace simplon\entities;
 
 class Option {
+  private $id;
+  private $text;
+  private $count;
+
+  public function __construct(int $id, string $text, int $count){
+    $this->id = $id;
+    $this->text = $text;
+    $this->count = $count;
+  }
+
+  public function get():array {
+    return [
+      "id" => $this->id,
+      "text" => $this->text,
+      "count" => $this->count
+    ];
+  }
 }
--- default/src/entities/Poll.php
+++ default/src/entities/Poll.php
@@ -3,5 +3,21 @@
 namespace simplon\entities;
 
 class Poll {
+  private $id;
+  private $title;
+  private $options;
 
+  public function __construct(int $id, string $title, array $options){
+    $this->id = $id;
+    $this->title = $title;
+    $this->options = $options;    
+  }
+
+  public function get():array{
+    return [
+      "id" => $this->id,
+      "title" => $this->title,
+      "options" => $this->options
+    ];
+  }
 }
